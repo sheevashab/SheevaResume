76a0242b804eb0b10a37e5a453392f7b9b5847f7
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-09-07 11:33:24 -0500

    first go around

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+const fs = require('fs')
+
+fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
+    return data
+  }
+})
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a mixed resume - part me, Sheeva Shabahang, and part Hammad Malik. Thanks Hammad!
